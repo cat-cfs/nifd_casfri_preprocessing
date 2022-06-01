45a801dbf88349f0f3de709a75ea918f5831a6bc
Scott Morken <smorken@nrcan.gc.ca>
2022-06-01 15:51:30 -0700

    fixes

--- nifd_casfri_preprocessing/sql/__init__.py
+++ nifd_casfri_preprocessing/sql/__init__.py
@@ -15,6 +15,6 @@ def _get_script_dir() -> str:
     return os.path.dirname(os.path.realpath(__file__))
 
 
-def get_inventory_id_fitered_query(name: str, inventory_id: int) -> str:
+def get_inventory_id_fitered_query(name: str, inventory_id: str) -> str:
     with open(os.path.join(_get_script_dir(), f"{name}.sql")) as fp:
-        return fp.read().format(inventory_id=int(inventory_id))
+        return fp.read().format(inventory_id=inventory_id)
