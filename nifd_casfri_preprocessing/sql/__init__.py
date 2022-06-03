6bc828b47598faea5e0d6050fb468bcc45dd6b20
Scott <scottm361@gmail.com>
2022-06-03 13:30:31 -0700

    add unfiltered method (for geopackage)

--- nifd_casfri_preprocessing/sql/__init__.py
+++ nifd_casfri_preprocessing/sql/__init__.py
@@ -18,3 +18,9 @@ def _get_script_dir() -> str:
 def get_inventory_id_fitered_query(name: str, inventory_id: str) -> str:
     with open(os.path.join(_get_script_dir(), f"{name}.sql")) as fp:
         return fp.read().format(inventory_id=inventory_id)
+
+
+def get_unfiltered_query(name: str):
+    if name not in NAMES:
+        raise ValueError()
+    return f"SELECT * from {name}"
