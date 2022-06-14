19769d277b05daec0e0d5932d7167417d4354ecb
Scott <scottm361@gmail.com>
2022-06-14 16:25:29 -0700

    add queries to extract raster information from casfri geo

---
+++ nifd_casfri_preprocessing/sql/gdal_rasterization_lookup.sql
@@ -0,0 +1,1 @@
+SELECT geo_all.cas_id, ROW_NUMBER() OVER (ORDER BY geo_all.cas_id) AS raster_id FROM geo_all inner join cas_all on cas_all.cas_id = geo_all.cas_id where cas_all.inventory_id = '{inventory_id}'
