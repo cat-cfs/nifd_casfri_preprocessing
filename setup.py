78e7891ca4cea792b8de5124e7c7074f51303243
Scott <scottm361@gmail.com>
2022-06-17 10:23:49 -0700

    new ver 0.6.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ console_scripts = [
 
 setup(
     name="nifd_casfri_preprocessing",
-    version="0.6.2",
+    version="0.6.3",
     description="nifd casfri preprocessing scripts",
     long_description=long_description,
     long_description_content_type="text/markdown",
